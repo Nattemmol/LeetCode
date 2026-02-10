1c6ae08c95c8281ddf2e4254ba84944616760436
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-02-10 17:13:47 +0300

    Time: 204 ms (51.72%) | Memory: 41.2 MB (5.17%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.py
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.py
@@ -1,33 +1,6 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        '''ans = []
-        changed.sort()
-        i = 0
-        n = len(changed)
-
-        if len(changed) == 1:
-            return []
-        if changed[i] == 0 and changed.count(0)%2 == 1:
-                return []
-        elif changed[i] == 0 and changed.count(0)%2 == 0:
-            idx = changed[::-1].index(0)
-            changed = changed[idx+1:]
-        while i < n:
-            
-            if changed[i]*2 in changed:
-
-                dob = changed.index(int(changed[i]*2))
-                ans.append(changed[i])
-                temp = changed[-1]
-                changed[-1] = changed[dob]
-                changed[dob] = temp
-                changed.pop()
-                i += 1
-                n -= 1
-                changed.sort()
-            else:
-                return []
-        return ans'''
+        
         count = Counter(changed)
         ans = []
         if len(changed) % 2 == 1:
