9ec9f4b1cbfdd63415882acae0bbfd578b69b112
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-02-12 16:09:23 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (46.01%) - LeetSync

--- 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.py
+++ 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.py
@@ -5,13 +5,14 @@ class Solution:
         if two.count("x") % 2 == 1 or two.count("y") % 2 == 1:
             return -1
         count_xy, count_yx = 0,0
+
+        
         for char_s1, char_s2 in zip(s1, s2):
             if char_s1 == "x" and char_s2 == "y":
                 count_xy += 1
             elif char_s1 == "y" and char_s2 == "x":
                 count_yx += 1
-        print(count_xy)
-        print(count_yx)
+        
         if count_xy % 2 == 0:
             ans += count_xy // 2
         if count_yx % 2 == 0:
