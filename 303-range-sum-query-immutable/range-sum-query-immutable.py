6f501dee9ad3c5d6ecaa89bf44ccce4f24e2f2e4
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-03-02 16:29:56 +0300

    Time: 0 ms (100.00%) | Memory: 22.9 MB (15.95%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -0,0 +1,13 @@
+class NumArray:
+    """
+    This class pre-calculates a prefix sum array to answer
+    range sum queries in constant time.
+    """
+    def __init__(self, nums: List[int]):
+
+        self.prefixSum = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            self.prefixSum[i + 1] = self.prefixSum[i] + nums[i]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefixSum[right + 1] - self.prefixSum[left]
