91c7e446ce60750524db8ba98426e09a1e84e4c4
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-03-10 16:22:57 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (89.92%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,14 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        dicts = {n:i for i,n in enumerate(nums1)}
-        print(dicts)
+        
         stack=[]
-        res = [-1] * len(nums1)
-        for i in range(len(nums2)):
-            while stack and nums2[i]>stack[-1]:
-                val = stack.pop()
-                ptr = dicts[val]
-                res[ptr] = nums2[i]
-            if nums2[i] in dicts:
-                stack.append(nums2[i])
-        return res
+        greater = defaultdict(lambda:-1)
+
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                top = stack.pop()
+                greater[top] = num
+            stack.append(num)
+        
+        ans = [greater[num] for num in nums1]
+
+        return ans
