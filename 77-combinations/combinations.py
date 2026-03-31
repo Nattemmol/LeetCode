d83e602fae17920f5381e87788ad9f3a3f24118e
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-03-31 16:21:01 +0300

    Time: 105 ms (72.63%) | Memory: 61.4 MB (65.64%) - LeetSync

---
+++ 77-combinations/combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        def comb(start, temp):
+            if len(temp) == k:
+                ans.append(temp[:])
+                return
+            
+            for i in range(start, n+1):
+                temp.append(i)
+                comb(i+1, temp)
+                temp.pop()
+        comb(1, [])
+        return ans
