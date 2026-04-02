3182ed9dcbeb094462d2b07962e27916738714a5
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-04-02 16:07:38 +0300

    Time: 11 ms (45.91%) | Memory: 19.7 MB (8.08%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        pos_diagonal = set()
+        neg_diagonal = set()
+        
+        ans = []
+        temp = [['.']* n for _ in range(n)]
+
+        def place(r):
+            if r == n: 
+                ans.append(["".join(tmp[:]) for tmp in temp])
+            
+            for c in range(n):
+                if c in col or (r+c) in pos_diagonal or (r-c) in neg_diagonal:
+                    continue
+                pos_diagonal.add(r+c)
+                neg_diagonal.add(r-c)
+                col.add(c)
+                temp[r][c] = 'Q'
+
+                place(r+1)
+
+                pos_diagonal.remove(r+c)
+                neg_diagonal.remove(r-c)
+                col.remove(c)
+                temp[r][c] = '.'
+
+        place(0)
+        return len(ans)
+
+
+
