25595b22c3a050079c1617d770faa2884c504604
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-04-28 13:16:10 -0300

    Time: 7 ms (28.29%) | Memory: 21 MB (34.35%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,30 +1,56 @@
+# class Solution:
+#     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+#         pre = defaultdict(list)
+#         pre_count = [0 for _ in range(numCourses)]
+
+#         ans = []
+
+#         for x,y in prerequisites:
+#             pre[y].append(x)
+#             pre_count[x] += 1
+
+#         for i in range(numCourses):
+#             if i not in pre:
+#                 pre[i] = []
+        
+#         print(pre,pre_count)
+        
+#         stack = []
+
+
+from collections import defaultdict
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        pre = defaultdict(list)
-        pre_count = [0 for _ in range(numCourses)]
+        
+        adj = defaultdict(list)
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+            
 
+        state = [0] * numCourses
         ans = []
 
-        for x,y in prerequisites:
-            pre[y].append(x)
-            pre_count[x] += 1
-        
-        queue = deque()
-        for i in range(len(pre_count)):
-            if pre_count[i] == 0:
-                queue.append(i)
-                ans.append(i)
-        
-        while queue:
-            node = queue.pop()
+        def dfs(course):
+            
+            if state[course] == 1:
+                return False
+                
+            if state[course] == 2:
+                return True
+            
+            state[course] = 1
+            
+            for pre in adj[course]:
+                if not dfs(pre):
+                    return False
+            
+            state[course] = 2
+            ans.append(course)
+            return True
 
-            for negh in pre[node]:
-                pre_count[negh] -= 1
-                if pre_count[negh] == 0:
-                    queue.append(negh)
-                    ans.append(negh)
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
         
-        if len(ans) < numCourses:
-            return []
-        else:
-            return ans
+        return ans
