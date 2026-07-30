a391e1403d75ce74aa4f6f8a348869012d39f78b
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-07-30 21:01:50 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (60.72%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,18 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
+        rob = [0] * len(nums)
+        if len(nums) == 1:
             return nums[0]
-        if n == 2:
-            return max(nums[0],nums[1])
-        amount = [0]*n
-        amount[0] = nums[0]
-        amount[1] = nums[1]
-        amount[2] = nums[0] + nums[2]
-        ans = 0
-        i = 3
-        while i < len(nums):
-            amount[i] = max(nums[i]+amount[i-2],nums[i]+amount[i-3])
-            i += 1
-        print(amount)
-        return max(amount[n-1],amount[n-2])
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        rob[0],rob[1], rob[2] = nums[0], nums[1], nums[0]+nums[2]
+
+        for i in range(3, len(nums)):
+            rob[i] = nums[i] + max(rob[i-2],rob[i-3])
+        print(rob)
+        return max(rob[-1],rob[-2])
